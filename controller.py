d348f5d65aab19bfd322da26fdf149671f119246
matteo-giubergia <matteo.giuebrgia2003@gmail.com>
2025-03-21 12:24:22 +0100

    finito

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
 import time
+from time import sleep
+
 import flet as ft
 import model as md
 
@@ -20,7 +22,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWord(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " - "
+                        paroleErrate = paroleErrate + str(parola) + " "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
 
@@ -38,12 +40,52 @@ class SpellChecker:
                 parole = self._multiDic.searchWordDichotomic(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " - "
+                        paroleErrate = paroleErrate + str(parola) + " "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
             case _:
                 return None
 
+    def handleSpellCheck(self,e):
+        self._view._txtOut.clean()
+        if self._view._txtIn.value =="":
+            self._view._txtOut.controls.append(ft.Text("Add a sentence!", color="red"))
+            self._view.page.update()
+            sleep(1)
+            self._view._txtOut.clean()
+            self._view.page.update()
+            return
+        if self._view._dd_scegliLingua.value is None:
+            self._view._txtOut.controls.append(ft.Text("Select a language!", color="red"))
+            self._view.page.update()
+            sleep(1)
+            self._view._txtOut.clean()
+            self._view.page.update()
+            return
+        if self._view._ddScegliMetodo.value is None:
+            self._view._txtOut.controls.append(ft.Text("Choose a modality!", color="red"))
+            self._view.page.update()
+            sleep(1)
+            self._view._txtOut.clean()
+            self._view.page.update()
+            return
+        testo=self._view._txtIn.value
+        lingua = self._view._dd_scegliLingua.value
+        modalita = self._view._ddScegliMetodo.value
+
+        paroleSbagliate, tempo = self.handleSentence(testo, lingua, modalita)
+
+        self._view._txtOut.clean()
+
+        if not paroleSbagliate == " - ":
+            self._view._txtOut.controls.append(ft.Text(value=f"Frase inserita: {testo}\nParole errate: {paroleSbagliate}\nTempo richiesto dalla ricerca: {tempo}"))
+            self._view.page.update()
+        else:
+            self._view._txtOut.controls.append(ft.Text(value=f"Everything is correct!\nFrase inserita: {testo}\nTempo richiesto dalla ricerca: {tempo}"))
+            self._view.page.update()
+
+        self._view._txtIn.value = ""
+        self._view.page.update()
 
     def printMenu(self):
         print("______________________________\n" +
--- view.py
+++ view.py
@@ -1,6 +1,7 @@
+from time import sleep
 import flet as ft
 
-class View(object):
+class View(object):                             # DA QUI GESTISCO L'INTERFACCIA GRAFICA
     def __init__(self, page: ft.Page):
         # Page
         self.page = page
@@ -14,6 +15,7 @@ class View(object):
         self.__theme_switch = None
 
         # define the UI elements and populate the page
+        self._txtOut = None
 
     def add_content(self):
         """Function that creates and adds the visual elements to the page. It also updates
@@ -22,20 +24,35 @@ class View(object):
         self.__title = ft.Text("TdP 2024 - Lab 04 - SpellChecker ++", size=24, color="blue")
         self.__theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
         self.page.controls.append(
-            ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
+            ft.Row(spacing=30, controls=[self.__theme_switch, self.__title ],
                    alignment=ft.MainAxisAlignment.START)
         )
 
         # Add your stuff here
+        self._dd_scegliLingua = ft.Dropdown(label="Select Language",
+                                            options=[ft.dropdown.Option("italian"),ft.dropdown.Option("english"),ft.dropdown.Option("spanish")])
 
-        self.page.add([])
+        self._ddScegliMetodo = ft.Dropdown(label="Search Modality",
+                                      options=[ft.dropdown.Option("Default"),ft.dropdown.Option("Linear"),ft.dropdown.Option("Dichotomic")], expand = 1)
+        self._txtIn = ft.TextField(label="Add your sentence here",hint_text="Text", value="", expand = 1)
+        self._btn = ft.ElevatedButton("SpellCheck", width = 100, on_click=self.__controller.handleSpellCheck)#da mettere in  controller
 
+        self._txtOut = ft.ListView(expand=1)# sara giusto?      non so
+
+        row1 = ft.Row([self._dd_scegliLingua])
+        row2 = ft.Row([self._ddScegliMetodo, self._txtIn, self._btn])
+        row3=ft.Row([self._txtOut])
+
+
+        self.page.add(row1,row2, row3)
         self.page.update()
 
     def update(self):
         self.page.update()
+
     def setController(self, controller):
         self.__controller = controller
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
